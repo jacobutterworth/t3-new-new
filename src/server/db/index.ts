f8ad89edfd1c1a56517727d9be4b703d981a5799
framework-jacobutterworth <jacob.b@weareframework.co.uk>
2025-04-08 20:31:39 +0100

    fix: neon

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { drizzle as drizzleNeonAdapter } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 
 import { env } from "~/env";
 import * as schema from "./schema";
@@ -8,11 +10,6 @@ import * as schema from "./schema";
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDb = globalThis as unknown as {
-	conn: postgres.Sql | undefined;
-};
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
-
-export const db = drizzle(conn, { schema });
+const sql = neon(env.DATABASE_URL);
+export const db = drizzleNeonAdapter(sql);
